bf4d1dd3f10563cbd86c189bc0c77385daee22e0
kielkow <matheuskiel@fiorifer.com.br>
2022-08-18 12:05:35 -0300

    feat: createProduct service

--- purchases/src/http/graphql/resolvers/products.resolver.ts
+++ purchases/src/http/graphql/resolvers/products.resolver.ts
@@ -1,8 +1,9 @@
-// import { UseGuards } from '@nestjs/common';
-import { Resolver, Query } from '@nestjs/graphql';
+import { UseGuards } from '@nestjs/common';
+import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
 
-// import { AuthorizationGuard } from 'src/http/auth/authorization.guard';
+import { AuthorizationGuard } from 'src/http/auth/authorization.guard';
 import { ProductsService } from 'src/services/products.service';
+import { CreateProductInput } from '../inputs/create-product-input';
 
 import { Product } from '../models/product';
 
@@ -11,8 +12,13 @@ export class ProductsResolver {
   constructor(private productsService: ProductsService) {}
 
   @Query(() => [Product])
-  // @UseGuards(AuthorizationGuard)
   products() {
     return this.productsService.listAllProducts();
+  }
+
+  @UseGuards(AuthorizationGuard)
+  @Mutation(() => Product)
+  createProduct(@Args('data') data: CreateProductInput) {
+    return this.productsService.createProduct(data);
   }
 }
