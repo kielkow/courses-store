c72d1800cee5d438962a0208b8a278bd1b8fb7b0
kielkow <matheuskiel@fiorifer.com.br>
2022-08-18 11:32:56 -0300

    refact: remove auth from products route

--- purchases/src/http/graphql/resolvers/products.resolver.ts
+++ purchases/src/http/graphql/resolvers/products.resolver.ts
@@ -1,7 +1,7 @@
-import { UseGuards } from '@nestjs/common';
+// import { UseGuards } from '@nestjs/common';
 import { Resolver, Query } from '@nestjs/graphql';
 
-import { AuthorizationGuard } from 'src/http/auth/authorization.guard';
+// import { AuthorizationGuard } from 'src/http/auth/authorization.guard';
 import { ProductsService } from 'src/services/products.service';
 
 import { Product } from '../models/product';
@@ -11,7 +11,7 @@ export class ProductsResolver {
   constructor(private productsService: ProductsService) {}
 
   @Query(() => [Product])
-  @UseGuards(AuthorizationGuard)
+  // @UseGuards(AuthorizationGuard)
   products() {
     return this.productsService.listAllProducts();
   }
