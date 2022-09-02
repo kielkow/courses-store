d7ef46374b6d624d2285d911b4f4cd3ac4a20ae1
kielkow <matheuskiel@fiorifer.com.br>
2022-09-02 11:12:18 -0300

    feat: get enrollment by course and student id

--- classroom/src/services/enrollments.service.ts
+++ classroom/src/services/enrollments.service.ts
@@ -1,6 +1,11 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/database/prisma/prisma.service';
 
+interface GetByCourseAndStudentIdParams {
+  courseId: string;
+  studentId: string;
+}
+
 @Injectable()
 export class EnrollmentsService {
   constructor(private prisma: PrismaService) {}
@@ -19,6 +24,22 @@ export class EnrollmentsService {
   listEnrollmentsByStudentId(studentId: string) {
     return this.prisma.enrollment.findMany({
       where: {
+        studentId,
+        canceledAt: null,
+      },
+      orderBy: {
+        createdAt: 'desc',
+      },
+    });
+  }
+
+  getByCourseAndStudentId({
+    courseId,
+    studentId,
+  }: GetByCourseAndStudentIdParams) {
+    return this.prisma.enrollment.findFirst({
+      where: {
+        courseId,
         studentId,
         canceledAt: null,
       },
